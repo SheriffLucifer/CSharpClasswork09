1fdd03d670aec18ccdcef77c2a59c4e8cf315737
Oleg <olegashepelev@gmail.com>
2023-01-31 00:12:33 +0600

    Complete third & fourth tasks

---
+++ 03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+
+
+Console.Clear();
+int number = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int sumNumbers = RecurrSum(number);
+Console.WriteLine($"{number} -> {sumNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int RecurrSum(int n3)
+{
+    if (n3 == 0) return 0;
+    else
+    {
+        return n3 % 10 + RecurrSum(n3 / 10);
+    }
+}
---
+++ 04/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+Console.Clear();
+int x = GetNumberFromUser("Введите целое число M: ", "Ошибка ввода!");
+int y = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+int sumNumbers = MultReccur(x, y);
+Console.WriteLine($"M = {x}; N = {y} -> {sumNumbers}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int MultReccur(int x, int y)
+{
+    if (y == 1) return x;
+    else
+    {
+        return x * MultReccur(x, y - 1);
+    }
+}
