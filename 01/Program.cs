929dcee7afc6957fc1d72fc8f38343d44e76e2dc
Oleg <olegashepelev@gmail.com>
2023-01-30 23:54:11 +0600

    Complete first task

---
+++ 01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от 1 до N.
+
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+string sumNumbers = ShowIntegers(1, num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+string ShowIntegers(int start, int end)
+{
+    if (start == end)
+    {
+        return end.ToString();
+    }
+    else return start + " " + ShowIntegers(start + 1, end);
+}
